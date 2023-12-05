3a3e07be6b1ce65c690dd4df473ab1e44b85deec
Martijn Gussekloo <1426964+mgussekloo@users.noreply.github.com>
2023-12-05 13:53:02 +0100

    Update Indexer.php

--- src/Indexer.php
+++ src/Indexer.php
@@ -10,82 +10,82 @@ class Indexer
 {
 	public $models;
 
-    public function __construct($models)
-    {
-    	$this->models = $models;
-    }
+	public function __construct($models = null)
+	{
+		$this->models = $models;
+	}
 
-    public function buildRow($facet, $model, $value)
-    {
-        return [
-            'facet_slug' => $facet->getSlug(),
-            'subject_id' => $model->id,
-            'value' => $value,
-        ];
-    }
+	public function buildRow($facet, $model, $value)
+	{
+		return [
+			'facet_slug' => $facet->getSlug(),
+			'subject_id' => $model->id,
+			'value' => $value,
+		];
+	}
 
-    public function insertRows($rows)
-    {
-        return FacetRow::insert(array_values($rows));
-    }
+	public function insertRows($rows)
+	{
+		return FacetRow::insert(array_values($rows));
+	}
 
-    public function resetIndex()
-    {
-        DB::table('facetrows')->truncate();
+	public function resetIndex()
+	{
+		DB::table('facetrows')->truncate();
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function buildIndex($models = null)
-    {
-    	$this->models = $models;
+	public function buildIndex($models = null)
+	{
+		$this->models = $models;
 
-        if (! is_null($this->models) && $this->models->isNotEmpty()) {
-            $subjectType = $this->models->first()::class;
+		if (! is_null($this->models) && $this->models->isNotEmpty()) {
+			$subjectType = $this->models->first()::class;
 
-            $facets = FacetFilter::getFacets($subjectType);
+			$facets = FacetFilter::getFacets($subjectType);
 
-            $now = now();
-            $rows = [];
-            foreach ($this->models as $model) {
-                foreach ($facets as $facet) {
-                    $values = [];
+			$now = now();
+			$rows = [];
+			foreach ($this->models as $model) {
+				foreach ($facets as $facet) {
+					$values = [];
 
-                    $fields = explode('.', (string) $facet->fieldname);
+					$fields = explode('.', (string) $facet->fieldname);
 
-                    if (count($fields) == 1) {
-                        $values = collect([$model->{$fields[0]}]);
-                    } else {
-                        $last_key = array_key_last($fields);
+					if (count($fields) == 1) {
+						$values = collect([$model->{$fields[0]}]);
+					} else {
+						$last_key = array_key_last($fields);
 
-                        $values = collect([$model]);
-                        foreach ($fields as $key => $field) {
-                            $values = $values->pluck($field);
-                            if ($key !== $last_key) {
-                                $values = $values->flatten(1);
-                            }
-                        }
-                    }
+						$values = collect([$model]);
+						foreach ($fields as $key => $field) {
+							$values = $values->pluck($field);
+							if ($key !== $last_key) {
+								$values = $values->flatten(1);
+							}
+						}
+					}
 
-                    foreach ($values as $value) {
-                        if (is_null($value)) {
-                            continue;
-                        }
+					foreach ($values as $value) {
+						if (is_null($value)) {
+							continue;
+						}
 
-                        $uniqueKey = implode('.', [$facet->getSlug(), $model->id, $value]);
-                        $row = $this->buildRow($facet, $model, $value);
-                        $row = array_merge([
-                            'created_at' => $now,
-                            // 'updated_at' => null,
-                        ], $row);
-                        $rows[$uniqueKey] = $row;
-                    }
-                }
-            }
+						$uniqueKey = implode('.', [$facet->getSlug(), $model->id, $value]);
+						$row = $this->buildRow($facet, $model, $value);
+						$row = array_merge([
+							'created_at' => $now,
+							// 'updated_at' => null,
+						], $row);
+						$rows[$uniqueKey] = $row;
+					}
+				}
+			}
 
-            $this->insertRows($rows);
-        }
+			$this->insertRows($rows);
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 }
